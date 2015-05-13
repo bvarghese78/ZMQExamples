0092bf7359bc47bf9082702b895f755a40ff97e6
Benji Varghese <benjivarghese@gmail.com>
2015-05-12 21:44:15 -0500

    initial subscriber commit

---
+++ zmqSubscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+
+namespace zmqSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topic = "Transport1";
+            using (var context = NetMQContext.Create())
+            using (var subscriber = context.CreateSubscriberSocket())
+            {
+                subscriber.Connect("tcp://localhost:5556");
+                subscriber.Subscribe(topic);
+                Console.WriteLine("Subscriber socket connecting...");
+
+                while (true)
+                {
+                    string messageTopicReceived = subscriber.ReceiveString();
+                    string messageReceived = subscriber.ReceiveString();
+                    Console.WriteLine(messageReceived);
+
+                }
+            }
+        }
+    }
+}

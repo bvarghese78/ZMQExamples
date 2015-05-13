f4048dbd439e43c95d918af2dff91dc466df0deb
Benji Varghese <benjivarghese@gmail.com>
2015-05-12 21:52:09 -0500

    Publisher commit

---
+++ ZmqPublisher/ZmqPublisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZmqPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What would you like to run? pub/xpub: ");
+            string response = Console.ReadLine();
+
+            Publisher pub = new Publisher();
+
+            if (response.ToLower() == "pub")
+                pub.regularPublish();
+            else if (response.ToLower() == "xpub")
+                pub.XPublish();
+        }
+    }
+}
---
+++ ZmqPublisher/ZmqPublisher/Publisher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+using System.Threading;
+
+namespace ZmqPublisher
+{
+    public class Publisher
+    {
+        public void regularPublish()
+        {
+            using (var context = NetMQContext.Create())
+            using (var publisher = context.CreatePublisherSocket())
+            {
+                Console.WriteLine("Publisher socket binding...");
+                publisher.Options.SendHighWatermark = 1000;
+                publisher.Bind("tcp://localhost:5556");
+
+                for (var i = 1; i <= 100; i++)
+                {
+                    Console.WriteLine("Sending Message: " + i);
+
+                    if (i % 2 == 0)
+                        publisher.SendMore("Transport2").Send("From the publisher: " + i);
+                    else
+                        publisher.SendMore("Transport1").Send("From the publisher: " + i);
+
+                    Thread.Sleep(50);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        public void XPublish()
+        {
+            using (var context = NetMQContext.Create())
+            using (var publisher = context.CreateXPublisherSocket())
+            {
+                Console.WriteLine("XPublisher socket binding...");
+                publisher.Options.SendHighWatermark = 1000;
+                publisher.Bind("tcp://localhost:5556");
+
+                for (var i = 1; i <= 100; i++)
+                {
+                    Console.WriteLine("XPUB Sending Message: " + i);
+                    publisher.SendMore("Transport1").Send("XPublisher: " + i);
+                    Thread.Sleep(50);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        //public async Task Intermediary()
+        //{
+        //    Task T1 = new Task();
+
+        //    using (var context = NetMQContext.Create())
+        //    using (var xpubSocket = context.CreateXPublisherSocket())
+        //    using (var xsubSocket = context.CreateXSubscriberSocket())
+        //    {
+        //        xpubSocket.Bind("tcp://localhost:1234");
+        //        xsubSocket.Bind("tcp://localhost:5556");
+        //        Console.WriteLine("Intermediary started, and waiting for messages");
+
+        //        var proxy = new Proxy(xsubSocket, xpubSocket);
+
+        //    }
+        //}
+    }
+}
